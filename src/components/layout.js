7b46490cb9d1105a4beaba4bed933db2587546bf
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-07-11 11:35:40 +0200

    add a common layout among pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,53 @@ import { Global, css } from '@emotion/core';
 
 const Layout = ({ children }) => (
   <>
-    <Global styles={css``} />
+    <Global
+      styles={css`
+        * {
+          box-sizing: border-box;
+          margin: 0;
+        }
+
+        * + * {
+          margin-top: 1rem;
+        }
+
+        html,
+        body {
+          margin: 0;
+          color: #555;
+          font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Helvetica, Arial, sans-serif;
+          font-size: 18px;
+          line-height: 1.4;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          color: #222;
+          line-height: 1.1;
+
+          + * {
+            margin-top: 0.5rem;
+          }
+        }
+
+        strong {
+          color: #222;
+        }
+
+        li {
+          margin-top: 0.25rem;
+        }
+      `}
+    />
+    <header></header>
+    <main>{children}</main>
   </>
 );
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <div>Hello Frontend Masters!</div>
     <Link to="/about">Learn more about me &rarr;</Link>
-  </>
+  </Layout>
 );
